9c74a20e03f8c6fa6297cc2e738e8fac1da0e8f8
Yannick PÉROUX <yannick.peroux@getalma.eu>
2024-10-28 21:51:19 +0100

    fish: force the shell to be in english

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
     # remove greeting
     set -g fish_greeting
+    
+    # force the shell to be in english for my sanity
+    set -gx LANG en_US.UTF-8
 
     # map ctrl+backspace to remove previous word
     bind \cH backward-kill-word
