268467968258d44645dd23686675309e762b3fdf
Yannick PÉROUX <yannick.peroux@getalma.eu>
2024-10-29 10:30:53 +0100

    nvim: add e & u objects for mini.ai

--- dot_config/nvim/lua/plugins/mini-ai.lua
+++ dot_config/nvim/lua/plugins/mini-ai.lua
@@ -16,6 +16,11 @@ return {
         n_lines = 500,
         search_method = "cover_or_nearest",
         custom_textobjects = {
+          -- `e` a word in a CamelCase or snake_case string
+          e = { 
+            { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+            "^().*()$",
+          },
           -- `o` for blocks
           o = ai.gen_spec.treesitter({
             a = { "@block.outer", "@conditional.outer", "@loop.outer" },
@@ -25,6 +30,8 @@ return {
           f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
           -- `c` for classes
           c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+          -- `u` (usage) for function calls
+          u = ai.gen_spec.function_call(),
           -- `g` for whole buffer
           g = function()
             local from = { line = 1, col = 1 }
