d51d63348ef6194b4fbb1d608f736e49cd5f4b73
Yannick PÉROUX <yannick.peroux@getalma.eu>
2024-10-30 11:44:01 +0100

    Use vscode as $EDITOR when already in vscode

--- dot_config/private_fish/conf.d/env.fish
+++ dot_config/private_fish/conf.d/env.fish
@@ -1,3 +1,8 @@
 fish_add_path --append ~/.local/bin
 
-set -gx EDITOR nvim
+if test "$TERM_PROGRAM" = vscode
+    # if we are already in vscode, use code as the editor
+    set -gx EDITOR code --wait
+else
+    set -gx EDITOR nvim
+end
