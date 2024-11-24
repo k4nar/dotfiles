7930722559271e4d9a125ac72dfc4ef582364288
Yannick PÉROUX <yannick.peroux@gmail.com>
2024-11-24 12:46:25 +0100

    fish: add Homebrew to the path in MacOS

--- dot_config/private_fish/conf.d/env.fish
+++ dot_config/private_fish/conf.d/env.fish
@@ -1,5 +1,9 @@
 fish_add_path --append ~/.local/bin
 
+if test (uname) = Darwin
+    fish_add_path /opt/homebrew/bin
+end
+
 if test "$TERM_PROGRAM" = vscode
     # if we are already in vscode, use code as the editor
     set -gx EDITOR code --wait
